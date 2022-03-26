8f8b2e0c2e9c3d052e8e26353834af5e27099279
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-26 22:49:53 +0100

    .

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once("templates/header.php"); ?>
+
+
+
+
+
+<h1>Test</h1>
+
+
+
+
+
+<?php require_once("templates/footer.php"); ?>
---
+++ templates/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/styles.css?v=<?php print(date("Y.m.d.H.i.s")); ?>">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- <link rel="stylesheet"  href="/css/dark.css" disabled>
+    <link rel="stylesheet"  href="/css/light.css" disabled> -->
+    <!-- <link rel="icon" type="image/png" href="favicon.png" sizes="1024x1024" /> -->
+    <!-- <link rel="stylesheet" href="/css/cookiebanner.css"> -->
+    <title>Kolping Jugend Schorndorf</title>
+</head>
+
+<body>
