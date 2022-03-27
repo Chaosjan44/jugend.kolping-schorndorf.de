6e7453ad6fbd93471db7c1dc9861d1d4f3446486
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-27 21:04:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,20 @@
     <div id="carouselExampleFade" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="/media/wir.jpg" class="d-block w-100" style="height: 40vh; object-fit: cover; object-position: 100% 50%;" data-bs-interval="5000">
+                <img src="/media/wir.jpg" class="d-block w-100" style="height: 450px; object-fit: cover; object-position: 100% 50%;" data-bs-interval="5000">
             </div>
             <div class="carousel-item">
-                <img src="/media/Vorgarten.jpg" class="d-block w-100" style="height: 40vh; object-fit: cover; object-position: 100% 40%;" data-bs-interval="5000">
+                <img src="/media/Vorgarten.jpg" class="d-block w-100" style="height: 450px; object-fit: cover; object-position: 100% 40%;" data-bs-interval="5000">
             </div>
             <div class="carousel-item">
-                <img src="/media/jummysoup.jpg" class="d-block w-100" style="height: 40vh; object-fit: cover; object-position: 100% 40%;" data-bs-interval="5000">
+                <img src="/media/jummysoup.jpg" class="d-block w-100" style="height: 450px; object-fit: cover; object-position: 100% 40%;" data-bs-interval="5000">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <button class="carousel-control-prev justify-content-start px-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Letztes</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <button class="carousel-control-next justify-content-end px-3" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Nächstes</span>
         </button>
