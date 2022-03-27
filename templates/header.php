8d55a3b61271ab9cf1a5995835651579ea2ce5da
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-27 23:21:05 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -52,6 +52,6 @@ require_once("php/functions.php");
                 </li>
             </ul>
         </div>
+        <div class="kolping-orange" style="height: 10px;"></div>
     </div>
-    <div class="kolping-orange" style="height: 10px;"></div>
 </nav>
