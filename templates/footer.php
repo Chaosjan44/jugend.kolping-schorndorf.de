f80749800c67e41d70cc5d388c95c3eb2870f286
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-27 17:55:05 +0200

    .

--- templates/footer.php
+++ templates/footer.php
@@ -7,40 +7,40 @@
             <div class="col-2">
                 <ul>
                     <li>
-                        <a href="/" class="" style="text-decoration:none">> Start</a>
+                        <a href="/" class="ctext" style="text-decoration:none">> Start</a>
                     </li>
                     <li>
-                        <a href="/termine.php" class="" style="text-decoration:none">> Termine</a>
+                        <a href="/termine.php" class="ctext" style="text-decoration:none">> Termine</a>
                     </li>
                     <li>
-                        <a href="/angebote.php" class="" style="text-decoration:none">> Angebote</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-2">
-                <ul>
-                    <li>
-                        <a href="/about_us.php" class="" style="text-decoration:none">> Über uns</a>
+                        <a href="/angebote.php" class="ctext" style="text-decoration:none">> Angebote</a>
                     </li>
                 </ul>
             </div>
             <div class="col-2">
                 <ul>
                     <li>
-                        <a href="/datenschutz.php" class="" style="text-decoration:none">> Datenschutz</a>
+                        <a href="/about_us.php" class="ctext" style="text-decoration:none">> Über uns</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <ul>
+                    <li>
+                        <a href="/datenschutz.php" class="ctext" style="text-decoration:none">> Datenschutz</a>
                     </li>
                     <li>
-                        <a href="/impressum.php" class="" style="text-decoration:none">> Impressum</a>
+                        <a href="/impressum.php" class="ctext" style="text-decoration:none">> Impressum</a>
                     </li>
                     <li>
-                        <a href="/kontakt.php" class="" style="text-decoration:none">> Kontakt</a>
+                        <a href="/kontakt.php" class="ctext" style="text-decoration:none">> Kontakt</a>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="row justify-content-end">
-        <div class="col-4 text-center light">&copy; 2020 Kolpingfamilie Schorndorf e.V.</div>
+        <div class="col-4 text-center ctext light">&copy; 2020 Kolpingfamilie Schorndorf e.V.</div>
         <div class="col-4 d-flex justify-content-end">
                 <input onchange="toggleStyle()" class="styleswitcher" type="checkbox" name="switch" id="style_switch">
                 <label class="styleswitcherlabel" for="style_switch"></label>
