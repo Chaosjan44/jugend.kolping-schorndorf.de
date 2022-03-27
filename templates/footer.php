1ec7520204e71e43d29368de17508c3fec8e7018
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-27 22:02:37 +0200

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,45 +1,78 @@
-
-
-
-<footer class="container-fluid sticky-bottom footer py-3">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-2 text-center">
-                <ul class="px-0">
-                    <li>
-                        <a href="/" class="hoverlink ctext" style="text-decoration:none">> Start</a>
-                    </li>
-                    <li>
-                        <a href="/termine.php" class="hoverlink ctext" style="text-decoration:none">> Termine</a>
-                    </li>
-                    <li>
-                        <a href="/angebote.php" class="hoverlink ctext" style="text-decoration:none">> Angebote</a>
-                    </li>
-                </ul>
+<?php if (!isMobile()): ?>
+    <footer class="container-fluid sticky-bottom footer py-3">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-2 text-center">
+                    <ul class="px-0">
+                        <li>
+                            <a href="/" class="hoverlink ctext" style="text-decoration:none">Start</a>
+                        </li>
+                        <li>
+                            <a href="/termine.php" class="hoverlink ctext" style="text-decoration:none">Termine</a>
+                        </li>
+                        <li>
+                            <a href="/angebote.php" class="hoverlink ctext" style="text-decoration:none">Angebote</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-2 text-center">
+                    <ul class="px-0">
+                        <li>
+                            <a href="/datenschutz.php" class="hoverlink ctext text-center" style="text-decoration:none">Datenschutz</a>
+                        </li>
+                        <li>
+                            <a href="/impressum.php" class="hoverlink ctext" style="text-decoration:none">Impressum</a>
+                        </li>
+                        <li>
+                            <a href="/kontakt.php" class="hoverlink ctext" style="text-decoration:none">Kontakt</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-2 text-center">
+        </div>
+        <div class="row justify-content-end">
+            <div class="col-4 text-center ctext light">&copy; 2020 Kolpingfamilie Schorndorf e.V.</div>
+            <div class="col-4 d-flex justify-content-end">
+                <input onchange="toggleStyle()" class="styleswitcher" type="checkbox" name="switch" id="style_switch">
+                <label class="styleswitcherlabel" for="style_switch"></label>
+            </div>
+        </div>
+    </footer>
+<?php else: ?>
+    <footer class="container-fluid sticky-bottom footer py-3">
+        <div class="container">
+            <div class="row justify-content-center">
                 <ul class="px-0">
                     <li>
-                        <a href="/datenschutz.php" class="hoverlink ctext text-center" style="text-decoration:none">> Datenschutz</a>
+                        <a href="/" class="hoverlink ctext" style="text-decoration:none">Start</a>
                     </li>
                     <li>
-                        <a href="/impressum.php" class="hoverlink ctext" style="text-decoration:none">> Impressum</a>
+                        <a href="/termine.php" class="hoverlink ctext" style="text-decoration:none">Termine</a>
                     </li>
                     <li>
-                        <a href="/kontakt.php" class="hoverlink ctext" style="text-decoration:none">> Kontakt</a>
+                        <a href="/angebote.php" class="hoverlink ctext" style="text-decoration:none">Angebote</a>
+                    </li>
+                    <li>
+                        <a href="/datenschutz.php" class="hoverlink ctext text-center" style="text-decoration:none">Datenschutz</a>
+                    </li>
+                    <li>
+                        <a href="/impressum.php" class="hoverlink ctext" style="text-decoration:none">Impressum</a>
+                    </li>
+                    <li>
+                        <a href="/kontakt.php" class="hoverlink ctext" style="text-decoration:none">Kontakt</a>
                     </li>
                 </ul>
             </div>
         </div>
-    </div>
-    <div class="row justify-content-end">
-        <div class="col-4 text-center ctext light">&copy; 2020 Kolpingfamilie Schorndorf e.V.</div>
-        <div class="col-4 d-flex justify-content-end">
+        <div class="row justify-content-end">
+            <div class="col-8 text-center ctext light">&copy; 2020 Kolpingfamilie Schorndorf e.V.</div>
+            <div class="col-4 d-flex justify-content-end">
                 <input onchange="toggleStyle()" class="styleswitcher" type="checkbox" name="switch" id="style_switch">
                 <label class="styleswitcherlabel" for="style_switch"></label>
             </div>
-    </div>
-</footer>
+        </div>
+    </footer>
+<?php endif; ?>
 
 
 
