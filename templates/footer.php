2693e1a3e950b83fc5eb14f9aeca6cf5b9c48c39
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-27 14:26:32 +0200

    .

--- templates/footer.php
+++ templates/footer.php
@@ -35,7 +35,7 @@ $verdate ="24.03.2022";
         </div>
         <div class="row">
             <span class="text-center">&copy; 2020 Kolpingfamilie Schorndorf e.V.</span>
-            <div class="">
+            <div class="d-flex justify-content-end align-items-center text-end ctext">
                 <input onchange="toggleStyle()" class="styleswitcher" type="checkbox" name="switch" id="style_switch" <?php if (check_style() == "dark") {print("checked");}?>>
                 <label class="styleswitcherlabel" for="style_switch"></label>
             </div>
