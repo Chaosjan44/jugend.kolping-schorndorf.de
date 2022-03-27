9f211157b0656deacf512a19315ef25e64b9266d
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-27 23:25:55 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -27,33 +27,31 @@ require_once("php/functions.php");
 <body style="padding-right: 0px !important;">
 
 <nav class="navbar header-header navbar-expand-lg navbar-<?php print(check_style());?> cbg ctext sticky-top">
-    <div>
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-                <img src="/favicon<?php if (check_style() == "dark") { print("_dark"); }?>.svg" class="navbar-icon d-inline-block align-text-center pe-2">
-                <?php if (!isMobile()) {print("Kolpingjugend Schorndorf");} else {print("KJ Schorndorf");}?>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse cbg" tabindex="-1" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link ctext" aria-current="page" href="/termine.php">Termine</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle ctext" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Über uns</a>
-                        <ul class="dropdown-menu cbg2" aria-labelledby="offcanvasNavbarDropdown">
-                            <li><a class="dropdown-item ctext" href="/about_us/wir.php">Wir</a></li>
-                            <li><a class="dropdown-item ctext" href="/about_us/aktionen.php">Aktionen</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link ctext" href="#">Angebote</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+            <img src="/favicon<?php if (check_style() == "dark") { print("_dark"); }?>.svg" class="navbar-icon d-inline-block align-text-center pe-2">
+            <?php if (!isMobile()) {print("Kolpingjugend Schorndorf");} else {print("KJ Schorndorf");}?>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse cbg" tabindex="-1" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link ctext" aria-current="page" href="/termine.php">Termine</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle ctext" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Über uns</a>
+                    <ul class="dropdown-menu cbg2" aria-labelledby="offcanvasNavbarDropdown">
+                        <li><a class="dropdown-item ctext" href="/about_us/wir.php">Wir</a></li>
+                        <li><a class="dropdown-item ctext" href="/about_us/aktionen.php">Aktionen</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ctext" href="#">Angebote</a>
+                </li>
+            </ul>
         </div>
-    <div>
-    <div class="kolping-orange" style="height: 10px;"></div>
+    </div>
 </nav>
+<div class="kolping-orange" style="height: 10px; position: sticky;"></div>
