5045c10db46a1b56c291355045c6767c70741474
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-27 20:54:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php require_once("templates/header.php"); ?>
 <script src="/js/slider.js"></script>
-<div class="container-fluid">
+<div class="container-fluid px-0">
     <div id="carouselExampleFade" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="/media/wir.jpg" class="d-block w-100" data-bs-interval="5000">
+                <img src="/media/wir.jpg" class="d-block w-100" style="height: 20vh;" data-bs-interval="5000">
             </div>
             <div class="carousel-item">
-                <img src="/media/Vorgarten.jpg" class="d-block w-100" data-bs-interval="5000">
+                <img src="/media/Vorgarten.jpg" class="d-block w-100" style="height: 20vh;" data-bs-interval="5000">
             </div>
             <div class="carousel-item">
-                <img src="/media/jummysoup.jpg" class="d-block w-100" data-bs-interval="5000">
+                <img src="/media/jummysoup.jpg" class="d-block w-100" style="height: 20vh;" data-bs-interval="5000">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
