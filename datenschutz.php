7662dbc0b9df7f079e63a6a49c2510f8c06f58ed
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-28 21:34:04 +0200

    Update datenschutz.php

--- datenschutz.php
+++ datenschutz.php
@@ -3,10 +3,24 @@
     <div>
         <h1 class="display-3 text-center text-kolping-orange">Datenschutzerklärung</h1>
         <h3>1 . Datenschutz auf einen Blick</h3>
-        <h5>Allgemeine Hinweise</h5>
-        <p>
-            Die folgenden Hinweise geben einen einfachen Überblick darüber, was mit lhren
+        <h5 class="mb-1">Allgemeine Hinweise</h5>
+        <p class="mb-3">
+            Die folgenden Hinweise geben einen einfachen Überblick darüber, was mit lhren<br>
             personenbezogenen Daten passiert, wenn Sie unsere Website besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie persönlich identifiziert werden können.
+        </p>
+        <h5 class="mb-1">Datenerfassung auf unserer Website</h5>
+        <p class="mb-3">
+            Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber. Dessen<br>
+            Kontaktdaten können Sie dem <a class="hoverlink ctext">lmpressum</a> dieser Website entnehmen.
+        </p>
+        <h5 class="mb-1">Wie erfassen wir lhre Daten?</h5>
+        <p class="mb-3">
+            lhre Daten werden zum einen dadurch erhoben, dass Sie uns diese mitteilen. Hierbei kann es sich z.B. um Daten handeln, die Sie in ein Kontaktformular eingeben.<br>
+            Andere Daten werden automatisch beim Besuch der Website durch unsere lT-Systeme erfasst. Das sind vor allem technische Daten (z.B. Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die Erfassung dieser Daten erfolgt automatisch, sobald Sie unsere Webseite betreten.
+        </p>
+        <h5 class="mb-1">Wofür nutzen wir lhre Daten?</h5>
+        <p class="mb-3">
+            Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der Website zu gewährleisten. Andere Daten können zur Analyse lhres Nutzerverhaltens verwendet werden.
         </p>
     </div>
 </div>
