f93c27fb735f752c731b2cf928e94877fdb8c62b
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-28 21:57:42 +0200

    Update impressum.php

---
+++ impressum.php
@@ -0,0 +1,39 @@
+<?php require_once("templates/header.php"); ?>
+<div class="container py-3">
+    <div>
+        <h1 class="display-3 text-center text-kolping-orange">Impressum</h1>
+        <h5 class="mb-1">Angaben gemäß § 5 Telemediengesetz (TMG):</h5>
+        <p class="mb-3">
+            Kolpingsfamilie Schorndorf e. V.<br>
+            Lortzingstraße 44<br>
+            73614 Schorndorf
+        </p>
+        <h5 class="mb-1">Vertreten durch das Leitungsteam der Kolpingfamilie Schorndorf:</h5>
+        <p class="mb-3">
+            Alexander Merkes, Timo Salenbauch, Caroline Weiß<br>
+            E-Mail: <a href="mailto:leitungsteam@kolping-schorndorf.de" class="text-kolping-orange">leitungsteam@kolping-schorndorf.de</a><br>
+            Telefon: 0 71 81 - 48 13 78
+        </p>
+        <h5 class="mb-1">Eintragung im Vereinsregister:</h5>
+        <p class="mb-3">
+            Registergericht: Amtsgericht Schorndorf<br>
+            Registernummer:
+        </p>
+        <h5 class="mb-1">Umsatzsteuer-ID:</h5>
+        <p class="mb-3">
+            Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:<br>
+            82005/08707 FA Schorndorf
+        </p>
+        <h5 class="mb-1">Inhaltlich verantwortlich i.S.v. §55 Abs. 2 RStV:</h5>
+        <p class="mb-3">
+            Leitungsteam der Kolpingfamilie Schorndorf<br>
+            Lortzingstraße 44<br>
+            73614 Schorndorf<br>
+            E-Mail: <a href="mailto:leitungsteam@kolping-schorndorf.de" class="text-kolping-orange">leitungsteam@kolping-schorndorf.de</a><br>
+            Telefon: 0 71 81 - 48 13 78<br>
+            <br>
+            Nähere Hinweise zu unserem Datenschutz finden Sie unter diesem <a href="/datenschutz.php" class="text-kolping-orange">Link</a>.
+        </p>
+    </div>
+</div>
+<?php require_once("templates/footer.php"); ?>
