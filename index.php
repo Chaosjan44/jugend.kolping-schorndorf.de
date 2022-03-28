32d6cf45812457c7d261db4d63eb984ad3c18fcf
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-28 19:34:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,12 @@
                         <div class="col card cbg2">
                             <div class="row g-0">
                                 <div class="col-md-4">
+                                    <!-- temp replacement for image -->
+                                <svg class="bd-placeholder-img img-fluid rounded-start" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                    <title>Placeholder</title>
                                     <rect width="100%" height="100%" fill="#868e96"></rect>
+                                    <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text>
+                                </svg>
                                     <!-- <img src="..." class="img-fluid rounded-start" alt="..."> -->
                                 </div>
                                 <div class="col-md-8">
