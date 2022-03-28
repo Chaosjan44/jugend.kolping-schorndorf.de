e50b03c1e3ab4006bfeb2a5e2e886a02a71ce653
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-28 21:39:22 +0200

    Update datenschutz.php

--- datenschutz.php
+++ datenschutz.php
@@ -11,17 +11,48 @@
         <h5 class="mb-1">Datenerfassung auf unserer Website</h5>
         <p class="mb-3">
             Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber. Dessen<br>
-            Kontaktdaten können Sie dem <a class="hoverlink ctext">lmpressum</a> dieser Website entnehmen.
+            Kontaktdaten können Sie dem <a href="/impressum.php" class="text-kolping-orange">lmpressum</a> dieser Website entnehmen.
         </p>
         <h5 class="mb-1">Wie erfassen wir lhre Daten?</h5>
         <p class="mb-3">
             lhre Daten werden zum einen dadurch erhoben, dass Sie uns diese mitteilen. Hierbei kann es sich z.B. um Daten handeln, die Sie in ein Kontaktformular eingeben.<br>
-            Andere Daten werden automatisch beim Besuch der Website durch unsere lT-Systeme erfasst. Das sind vor allem technische Daten (z.B. Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die Erfassung dieser Daten erfolgt automatisch, sobald Sie unsere Webseite betreten.
+            Andere Daten werden automatisch beim Besuch der Website durch unsere lT-Systeme erfasst. 
+            Das sind vor allem technische Daten (z.B. Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). 
+            Die Erfassung dieser Daten erfolgt automatisch, sobald Sie unsere Webseite betreten.
         </p>
         <h5 class="mb-1">Wofür nutzen wir lhre Daten?</h5>
         <p class="mb-3">
             Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der Website zu gewährleisten. Andere Daten können zur Analyse lhres Nutzerverhaltens verwendet werden.
         </p>
+        <h5 class="mb-1">Welche Rechte haben Sie bezüglich lhrer Daten?</h5>
+        <p class="mb-3">
+            Sie haben jederzeit das Recht unentgeltlich Auskunft über Herkunft, Empfänger und Zweck lhrer gespeicherten personenbezogenen Daten zu erhalten. 
+            Sie haben außerdem ein Recht, die Berichtigung, Sperrung oder Löschung dieser Daten zu verlangen. 
+            Hierzu sowie zu weiteren Fragen zum Thema Datenschutz können Sie sich jederzeit unter der im <a href="/impressum.php" class="text-kolping-orange">lmpressum</a> angegebenen Adresse an uns wenden. 
+            Des Weiteren steht lhnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu.
+        </p>
+
+        <h3>2. Allgemeine Hinweise und Pflichtinformationen</h3>
+        <h5 class="mb-1">Datenschutz</h5>
+        <p class="mb-3">
+            Die Betreiber dieser Seiten nehmen den Schutz lhrer persönlichen Daten sehr ernst. 
+            Wir behandeln lhre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.<br>
+            Wenn Sie diese Website benutzen, werden verschiedene personenbezogene Daten erhoben.<br>
+            Personenbezogene Daten sind Daten, mit denen Sie persönlich identifiziert werden können. 
+            Die vorliegende Datenschutzerklärung erläutert, welche Daten wir erheben und wofür wir sie nutzen. Sie erläutert auch, wie und zu welchem Zweck das geschieht.<br>
+            Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken autweisen kann. 
+            Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+        </p>
+        <h5 class="mb-1">Hinweis zur verantwortlichen Stelle</h5>
+        <p class="mb-3">
+            Die verantwortliche Stelle für die Datenverarbeilung auf dieser Website ist:
+        </p>
+        <span class="placeholder col-3"></span>
+        <span class="placeholder col-2"></span>
+        <span class="placeholder col-2"></span>
+        <br>
+        <span class="placeholder col-2"></span>
+        <span class="placeholder col-3"></span>
     </div>
 </div>
 <?php require_once("templates/footer.php"); ?>
