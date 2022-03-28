46e3d48014a794918d51b394ac28126a8262b372
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-28 19:49:39 +0200

    .

--- index.php
+++ index.php
@@ -48,13 +48,7 @@
                         <div class="col card cbg2">
                             <div class="row g-0">
                                 <div class="col-md-4">
-                                    <!-- temp replacement for image -->
-                                <!-- <svg class="bd-placeholder-img img-fluid rounded-start" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image" preserveAspectRatio="xMidYMid slice" focusable="false">
-                                    <title>Placeholder</title>
-                                    <rect width="100%" height="100%" fill="#868e96"></rect>
-                                    <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text>
-                                </svg> -->
-                                    <img src="Verbandsspiellogo mit KJunterbunt 2021_transparent.png" class="img-fluid rounded-start">
+                                    <img src="/media/Verbandsspiellogo_mit_KJunterbunt_2021_transparent.png" class="img-fluid rounded-start">
                                 </div>
                                 <div class="col-md-8">
                                     <div class="card-body ctext">
