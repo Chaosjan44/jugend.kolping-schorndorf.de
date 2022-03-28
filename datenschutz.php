3ffaa9dba5cf948252f16e77d45736cf26689f8a
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-28 21:28:52 +0200

    Create datenschutz.php

---
+++ datenschutz.php
@@ -0,0 +1,13 @@
+<?php require_once("templates/header.php"); ?>
+<div class="container py-3">
+    <div>
+        <h1 class="display-3 text-center text-kolping-orange">Datenschutzerklärung</h1>
+        <h3>1 . Datenschutz auf einen Blick</h3>
+        <h5>Allgemeine Hinweise</h5>
+        <p>
+            Die folgenden Hinweise geben einen einfachen Überblick darüber, was mit lhren
+            personenbezogenen Daten passiert, wenn Sie unsere Website besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie persönlich identifiziert werden können.
+        </p>
+    </div>
+</div>
+<?php require_once("templates/footer.php"); ?>
