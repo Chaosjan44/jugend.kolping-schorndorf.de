4d4c9cb439a04bbbd0ade99d80fb6dc8b64fe11a
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-30 19:02:34 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -28,8 +28,8 @@ require_once("php/functions.php");
 <nav class="navbar header-header navbar-expand-lg navbar-<?php print(check_style());?> cbg ctext sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">
-            <img src="/favicon<?php if (check_style() == "dark") { print("_dark"); }?>.svg" class="navbar-icon d-inline-block align-text-center pe-2">
-            <?php if (!isMobile()) {print("Kolpingjugend Schorndorf");} else {print("KJ Schorndorf");}?>
+            <img src="/favicon<?php if (check_style() == "dark") { print("_dark"); }?>.svg" class="navbar-icon d-inline-block align-text-top pe-2">
+            <?php if (!isMobile()) {print("Kolpingjugend<br>Schorndorf");} else {print("KJ Schorndorf");}?>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
