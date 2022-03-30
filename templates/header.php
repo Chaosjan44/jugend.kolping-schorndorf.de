0747a1f8fd1e9de6392916abb1e425931ec6eaf8
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-30 20:05:55 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -29,7 +29,7 @@ require_once("php/functions.php");
     <div class="container-fluid">
         <a class="navbar-brand" href="/">
             <img src="/favicon<?php if (check_style() == "dark") { print("_dark"); }?>.svg" class="navbar-icon d-inline-block align-text-top pe-2">
-            <?php if (!isMobile()) {print("Kolpingjugend<br>Schorndorf");} else {print("KJ Schorndorf");}?>
+            <?php if (!isMobile()) {print("Kolpingjugend Schorndorf");} else {print("KJ Schorndorf");}?>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
