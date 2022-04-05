2979d9b955ebaeebf1083ca972b61fe33aa2e6e3
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-05 19:30:57 +0200

    .

--- about_us/wir.php
+++ about_us/wir.php
@@ -1,9 +1,9 @@
 <?php 
 chdir ($_SERVER['DOCUMENT_ROOT']);
-require_once("/templates/header.php"); ?>
+require_once("templates/header.php"); ?>
 <div class="container py-3">
     <div style="min-height: 80vh;">
         <h1 class="display-3 text-center mb-3 text-kolping-orange">Wir</h1>
     </div>
 </div>
-<?php require_once("/templates/footer.php"); ?>
+<?php require_once("templates/footer.php"); ?>
