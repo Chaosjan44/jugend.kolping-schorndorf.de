3fb29d83837fa79e9d471c72fbe890572d92c8ef
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-07 08:41:09 +0200

    .

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $blogentrys = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <h2 class="col display-6 ctext text-center">Nachrichten</h2>
                     <div class="col d-flex justify-content-center">
                         <div class="row row-cols-1">
-                            <?php foreach ($blogentrys as $blogentry): ?>
+                            <?php foreach ($blogentrys as $blogentry): ?> 
                                 <div class="col card cbg2 mb-3">
                                     <div class="row g-0">
                                         <div class="col-md-4">
