ad0243f6113dfbe364a7bc11198f9cb5db03c5b4
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-07 18:48:57 +0200

    Update blog.php

--- blog.php
+++ blog.php
@@ -32,7 +32,7 @@ require("templates/header.php");
     </div>
     <div class="clearfix py-3">
         <div class="col-md-6 float-md-end mb-3 ms-md-3">
-            <div class="card py-2 px-2 cbg2 d-flex justify-content-center">
+            <div class="card py-3 px-3 cbg2 d-flex justify-content-center">
                 <div id="carouselExampleDark" class="d-flex justify-content-center carousel <?php if (check_style() == "dark") { print("carousel-dark "); }?>slide" data-bs-ride="carousel">
                     <?php if($images == null):?>
                         <div class="carousel-inner">
