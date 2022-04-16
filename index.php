4cd7d75eb2f25ba00831f20827501a4eaa780df2
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-16 19:48:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <div class="col d-flex justify-content-center">
                             <div class="row row-cols-1">
                             <?php foreach ($events as $event): ?> 
-                                <div class="col card cbg2 py-3 px-3"> 
+                                <div class="col mb-3 card cbg2 py-3 px-3"> 
                                     <div class="row g-0 row-cols-2">
                                         <div class="col-3 d-flex justify-content-start align-items-center">
                                             <div class="card cbg text-size-larger py-3 px-3 align-items-center text-center">
@@ -126,7 +126,7 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                                 <?=date('M', strtotime($event['date']))?>
                                             </div>
                                         </div>
-                                        <div class="col-9+ justify-content-start align-items-center">
+                                        <div class="col-9d-flex  justify-content-start align-items-center">
                                             <div class="card-body ctext align-items-center">
                                                 <h5 class="card-title align-center text-break"><?=$event['title']?></h5>
                                             </div>
