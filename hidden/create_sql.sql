279606c63fcfcaf44ceebbe633e94606b2838f2b
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-16 17:44:50 +0200

    Update create_sql.sql

--- hidden/create_sql.sql
+++ hidden/create_sql.sql
@@ -1,19 +1,58 @@
-CREATE TABLE `blog_entrys` (
-    `blog_entrys_id` INT(10) AUTO_INCREMENT NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `prev_text` TEXT(500) NOT NULL,
-   	`text` MEDIUMTEXT NOT NULL,
-    `visible` TINYINT(1) DEFAULT 0,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  	`updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    PRIMARY KEY (`blog_entrys_id`)
+CREATE TABLE `users` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nachname` varchar(255) NOT NULL,
+  `vorname` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `loginperms` tinyint(1) NOT NULL DEFAULT 0,
+  `admin` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`)
+)
+
+CREATE TABLE `securitytokens` (
+  `securitytoken_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `identifier` varchar(255) NOT NULL,
+  `securitytoken` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`securitytoken_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 );
+
+CREATE TABLE `blog_entrys` (
+  `blog_entrys_id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `prev_text` text NOT NULL,
+  `text` mediumtext NOT NULL,
+  `visible` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by` int(10) NOT NULL,
+	PRIMARY KEY (`blog_entrys_id`),
+    FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`)
+);
+
 CREATE TABLE `blog_images` (
-    `blog_images_id` INT(10) AUTO_INCREMENT NOT NULL,
-    `blog_entrys_id` INT(10) NOT NULL,
-    `source` VARCHAR(255) NOT NULL,
-    `alt` TEXT(200),
-   	`prev_img` TINYINT(1) DEFAULT 0,
-    PRIMARY KEY (`blog_images_id`),
-    FOREIGN KEY (`blog_entrys_id`) REFERENCES `blog_entrys` (`blog_entrys_id`)
+  `blog_images_id` int(10) NOT NULL AUTO_INCREMENT,
+  `blog_entrys_id` int(10) NOT NULL,
+  `source` varchar(255) NOT NULL,
+  `alt` text DEFAULT NULL,
+  `prev_img` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`blog_images_id`),
+  FOREIGN KEY (`blog_entrys_id`) REFERENCES `blog_entrys` (`blog_entrys_id`)
+);
+
+CREATE TABLE `events` (
+    `events_id` INT(10) NOT NULL AUTO_INCREMENT,
+    `date` date NOT NULL,
+    `datetime_from` datetime NOT NULL,
+    `datetime_to` datetime NOT NULL,
+    `title` varchar(255) NOT NULL,
+    `text` mediumtext NOT NULL,
+    `visible` tinyint(1) NOT NULL DEFAULT 0,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `created_by` INT(10) NOT NULL,
+    PRIMARY KEY (`events_id`),
+    FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`)
 );
