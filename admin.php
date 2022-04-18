eae6142b3efafd146cb99760d2dcdee4426d7d8c
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-18 15:14:18 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -13,12 +13,12 @@ require_once("templates/header.php"); ?>
             <div class="card-body text-center">
                 <h1 class="card-title display-3 text-center mb-0 text-kolping-orange">Admin Bereich</h1>
                 <div class="card-text">
-                    <button class="btn btn-primary my-3" type="button" onclick="window.location.href = 'admin/blog.php';">Blogs</button>
-                    <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/termine.php';">Termine</button>
+                    <button class="btn btn-primary mx-3" type="button" onclick="window.location.href = 'admin/blog.php';">Blogs</button>
+                    <button class="btn btn-primary mx-3" type="button" onclick="window.location.href = 'admin/termine.php';">Termine</button>
                     <?php if ($user['admin'] == "1"): ?>
-                        <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/user.php';">User</button>
-                        <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/wir.php';">Wir</button>
-                        <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/angebote.php';">Angebote</button>
+                        <button class="btn btn-primary mx-3" type="button" onclick="window.location.href = 'admin/user.php';">User</button>
+                        <button class="btn btn-primary mx-3" type="button" onclick="window.location.href = 'admin/wir.php';">Wir</button>
+                        <button class="btn btn-primary mx-3" type="button" onclick="window.location.href = 'admin/angebote.php';">Angebote</button>
                     <?php endif; ?>
                 </div>
             </div>
