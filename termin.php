d8da128f3b602b1e05083404dbca5528b16e29fe
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-18 20:24:55 +0200

    Create termin.php

---
+++ termin.php
@@ -0,0 +1,65 @@
+<?php
+require_once("php/functions.php");
+
+if (!isset($_GET["id"])) {
+    header("location: events.php");
+    exit;
+}
+
+$stmt = $pdo->prepare('SELECT * FROM events where events_id  = ?');
+// bindValue will allow us to use integer in the SQL statement, we need to use for LIMIT
+$stmt->bindValue(1, $_GET["id"], PDO::PARAM_INT);
+$stmt->execute();
+if ($stmt->rowCount() != 1) {
+    error_log($stmt->rowCount());
+    header("location: termine.php");
+    exit;
+}
+$event = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $pdo->prepare('SELECT vorname, nachname FROM users where user_id = ?');
+$stmt->bindValue(1, $event[0]['created_by'], PDO::PARAM_INT);
+$stmt->execute();
+$creator = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+require("templates/header.php");
+?>
+<div class="container-xxl py-3" style="min-height: 80vh;">
+    <div class="row">
+        <h1 class="display-4 text-center mb-3 text-kolping-orange"><?=$event[0]["title"]?></h1>
+    </div>
+    <div class="clearfix">
+        <div class="col-6 mb-3">
+            <div class="card cbg2 py-3 px-3">
+                <div class="row g-0">
+                    <div class="col-md-2 d-flex justify-content-start align-items-center">
+                        <div class="card cbg text-size-larger py-3 px-3 align-items-center text-center">
+                            <?=date('d', strtotime($event['date']))?>
+                            <br>
+                            <?=date('M', strtotime($event['date']))?>
+                        </div>
+                    </div>
+                    <div class="col-md-10 d-flex justify-content-start align-items-center">
+                        <div class="card-body ctext align-items-center">
+                            <h3 class="card-title align-center"><?=$event['title']?></h3>
+                        </div>
+                    </div>
+                    <a href="/termin.php?id=<?=$event['events_id']?>" class="stretched-link"></a>
+                </div>
+            </div>
+        </div>
+        <?=$event[0]["text"]?>
+    </div>
+    <div class="row justify-content-between">
+        <div class="col d-flex justify-content-start text-start ctext text-size-large">
+            <?=$creator[0]['vorname']?> <?=$creator[0]['nachname']?>
+        </div>
+        <div class="col d-flex justify-content-end text-end ctext text-size-large">
+            <?=date('d.m.Y H:i', strtotime($event[0]['created_at']))?>
+        </div>
+    </div>
+</div>
+
+<?php
+include_once("templates/footer.php")
+?>
