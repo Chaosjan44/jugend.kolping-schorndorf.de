b260f55928b1b0595cfe03556ccde66b5553c5b1
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-18 15:13:29 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -10,15 +10,17 @@ require_once("templates/header.php"); ?>
 <div class="container py-3">
     <div style="min-height: 80vh;">
         <div class="card cbg2 my-3 py-3 px-3">
-            <h1 class="card-title display-3 text-center mb-0 text-kolping-orange">Admin Bereich</h1>
-            <div class="card-body justify-content-center">
-                <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/blog.php';">Blogs</button>
-                <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/termine.php';">Termine</button>
-                <?php if ($user['admin'] == "1"): ?>
-                    <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/user.php';">User</button>
-                    <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/wir.php';">Wir</button>
-                    <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/angebote.php';">Angebote</button>
-                <?php endif; ?>
+            <div class="card-body text-center">
+                <h1 class="card-title display-3 text-center mb-0 text-kolping-orange">Admin Bereich</h1>
+                <div class="card-text">
+                    <button class="btn btn-primary my-3" type="button" onclick="window.location.href = 'admin/blog.php';">Blogs</button>
+                    <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/termine.php';">Termine</button>
+                    <?php if ($user['admin'] == "1"): ?>
+                        <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/user.php';">User</button>
+                        <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/wir.php';">Wir</button>
+                        <button class="btn btn-primary" type="button" onclick="window.location.href = 'admin/angebote.php';">Angebote</button>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>
