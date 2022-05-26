057582f9751c1f346b168be29c1011ccce9079c2
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-05-26 14:49:56 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -22,7 +22,6 @@ if (check_cookie()) {
     <link rel="stylesheet" href="/css/light.css" disabled>
     <script src="/js/custom.js"></script>
     <link rel="icon" type="image/svg" href="/favicon.svg" sizes="1024x1024" />
-    <link rel="stylesheet" href="/css/bootstrap-icons.css">
     <!-- <link rel="stylesheet" href="/css/cookiebanner.css"> -->
     <title>Kolpingjugend Schorndorf</title>
 </head>
