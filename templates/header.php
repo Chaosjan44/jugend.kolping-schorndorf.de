52dafd266d576018bd9665d6cbb8d21e5065351d
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-06-03 10:53:44 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -57,6 +57,10 @@ if (check_cookie()) {
         </div>
     </div>
 </nav>
+<script>
+    const myModal = new bootstrap.Modal('#cookieModal', options)
+    const modalToggle = document.getElementById('cookieModal'); myModal.show(modalToggle)
+</script>
 <button type="button" data-bs-toggle="modal" data-bs-target="#cookieModal">Cookies</button>
 <div class="modal fade" id="cookieModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
     <div class="modal-dialog cbg">
@@ -83,6 +87,6 @@ if (check_cookie()) {
 if (!check_cookie()):
 ?>
 <script type="text/javascript">
-    const modalToggle = document.getElementById('cookieModal'); modalToggle.show(modalToggle)
+    myModal.show(modalToggle)
 </script>
 <?php endif; ?>
