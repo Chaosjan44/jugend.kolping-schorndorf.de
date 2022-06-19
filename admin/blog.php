3c456e4c2d7d5f1907d5540357d5ebca28366a60
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-06-19 19:55:47 +0200

    Update blog.php

--- admin/blog.php
+++ admin/blog.php
@@ -2,7 +2,7 @@
 chdir ($_SERVER['DOCUMENT_ROOT']);
 require_once("php/functions.php");
 $user = check_user();
-if (!isset($user['id'])) {
+if (!isset($user)) {
     print("<script>location.href='/login.php'</script>");
     exit;
 }
