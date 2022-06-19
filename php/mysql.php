4b7ef3400f44dab4e7c205431115e09c96a80836
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-06-19 19:58:02 +0200

    Update mysql.php

--- php/mysql.php
+++ php/mysql.php
@@ -2,7 +2,7 @@
 require_once "config.php";
 
 try {
-  $pdo = new PDO('mysql:host=' . $ini_array["mysql"]["host"] . ';dbname=' . $ini_array["mysql"]["database"] . ';charset=utf8', $ini_array["mysql"]["user"], $ini_array["mysql"]["passwd"]);
+  $pdo = new PDO('mysql:host=' . $ini_array["mysql"]["host"] . ';dbname=' . $ini_array["mysql"]["database"] . ';charset=utf8', $ini_array["mysql"]["user"], $ini_array["mysql"]);
   //Connected successfully
 } catch(PDOException $e) {
   // If there is an error with the connection, stop the script and display the error.  
