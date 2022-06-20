f62df4721fb01e197622b24cb40436c0da8ec285
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-06-20 14:29:53 +0200

    Update blog.php

--- admin/blog.php
+++ admin/blog.php
@@ -66,7 +66,7 @@ if (isset($_POST['action'])) {
             if (isset($_POST[$var])) {
                 #del
                 $stmt = $pdo->prepare('DELETE FROM blog_images where blog_images_id = ? and blog_entrys_id = ?');
-                $stmt->bindValue(1, $x, PDO::PARAM_INT);
+                $stmt->bindValue(1, $_POST[$var], PDO::PARAM_INT);
                 $stmt->bindValue(2, $blog_entrys_id, PDO::PARAM_INT);
                 $result = $stmt->execute();
                 if (!$result) {
