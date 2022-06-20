7e60b2747b6d52475b3c9afe3dda0085308f8c00
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-06-20 08:28:51 +0200

    Update blog.php

--- admin/blog.php
+++ admin/blog.php
@@ -158,8 +158,7 @@ if (isset($_POST['action'])) {
                             <button type="button" class="btn btn-kolping ctext px-3" data-bs-toggle="modal" data-bs-target="#explainModal"><i class="fa-solid fa-circle-question"></i></button>
                         </div>
                         <div class="justify-content-end d-flex">
-                            <div class="input-group align-content-center mx-2">
-                                <span class="input-group-text" for="inputVisible">Visible</span>
+                            <div class="align-content-center mx-2">
                                 <input class="form-check-input mt-0 checkbox-kolping" type="checkbox" id="inputVisible" name="visible" <?=($entry[0]['visible']==1 ? 'checked':'')?>>
                             </div>
                             <input type="number" value="<?=$blog_entrys_id?>" name="blog_entrys_id" style="display: none;" required>
