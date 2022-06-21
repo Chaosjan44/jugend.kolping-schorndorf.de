77efced71537b65fa31d507382485574998e8e52
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-06-21 09:37:34 +0200

    Update blog.php

--- admin/blog.php
+++ admin/blog.php
@@ -74,6 +74,7 @@ if (isset($_POST['action'])) {
                     error('Datenbank Fehler!', pdo_debugStrParams($stmt));
                 }   
                 $delImg = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                error_log(print_r($delImg, true));
                 unlink($delImg['source']);
                 
                 $stmt = $pdo->prepare('DELETE FROM blog_images where blog_images_id = ? and blog_entrys_id = ?');
