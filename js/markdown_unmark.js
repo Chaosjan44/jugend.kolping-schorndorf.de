4fd171a2e82a0eda9111e6d507bfa03ea8af38c7
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-09-05 16:43:12 +0200

    Update markdown_unmark.js

--- js/markdown_unmark.js
+++ js/markdown_unmark.js
@@ -13,14 +13,14 @@ function unMark2(input) {
     input = "";
     for (i = 0; i < inputArray.length; i++) {
         console.log(i);
-        console.log(inputArray[i];)
+        console.log(inputArray[i]);
         if (i + 1 % 2 == 0 && i != 0) {
             inputArray[i].replace('<b>', '</b>');
         }
-        console.log(inputArray[i])
+        console.log(inputArray[i]);
         input += inputArray[i]
     }
-    input.replace()
+    input.replace();
 
 
 
