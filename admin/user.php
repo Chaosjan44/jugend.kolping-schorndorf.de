05cd690aedf3a35a980ff1daa351f3c6d823debb
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-09-07 18:12:33 +0200

    Update user.php

--- admin/user.php
+++ admin/user.php
@@ -101,7 +101,7 @@ if(isset($_POST['action'])) {
                                 <div class="input-group justify-content-center">
                                     <label for="loginrechte" class="input-group-text">Login Berechtigungen?</label>
                                     <div class="input-group-text">
-                                        <input value="remember-me" id="loginrechte" type="checkbox" name="loginrechte" value="0" class="form-check-input checkbox-kolping" checked>
+                                        <input value="remember-me" id="loginrechte" type="checkbox" name="loginrechte" value="<?=$user1[0]['loginperms']?>" class="form-check-input checkbox-kolping" checked>
                                     </div>
                                 </div>
                             </div>
@@ -109,7 +109,7 @@ if(isset($_POST['action'])) {
                                 <div class="input-group justify-content-center">
                                     <label for="adminrechte" class="input-group-text">Admin Berechtigungen?</label>
                                     <div class="input-group-text">
-                                        <input value="remember-me" id="adminrechte" type="checkbox" name="adminrechte" value="0" class="form-check-input checkbox-kolping" checked>
+                                        <input value="remember-me" id="adminrechte" type="checkbox" name="adminrechte" value="<?=$user1[0]['admin']?>" class="form-check-input checkbox-kolping" checked>
                                     </div>
                                 </div>
                             </div>
