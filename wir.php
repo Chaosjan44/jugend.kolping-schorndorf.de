18e7209ee214ac8610ad1569e5ef25c9e73e18e4
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-09-08 14:11:07 +0200

    Update wir.php

--- wir.php
+++ wir.php
@@ -8,12 +8,13 @@ require_once("templates/header.php"); ?>
             <span class="text-size-larger">
                 Die Kolpingjugend Schorndorf wurde 2019 von uns Gruppenleitenden wiedergegründet.<br>
                 Seitdem treffen wir uns in regelmäßigen Abständen als Jugendgruppe in unserem Kolpinghaus in Schorndorf.<br>
-                In unseren Gruppenstunden spielen wir Spiele, kochen zusammen, backen zusammen, machen Ausflüge und haben einfach eine menge Spaß.<br>
-                Seit unserer Wiedergründung renovieren wir zudem unseren Jugendraum und machen in <a href="https://www.instagram.com/explore/tags/kolpingfresh/" class="text-size-larger link">#kolpingfresh</a>.<br>
+                In unseren Gruppenstunden spielen wir Spiele, kochen zusammen, backen zusammen, machen Ausflüge und haben einfach eine Menge Spaß.<br>
+                Seit unserer Wiedergründung renovieren wir zudem unseren Jugendraum und machen in <a href="https://www.instagram.com/explore/tags/kolpingfresh/" class="text-size-large link">#kolpingfresh</a>.<br>
                 <br>
-                Zusätzlich zu unseren Gruppenstunden nehmen wir auch an Aktivitäten der <a href="https://jugend.kolping-dvrs.de" class="text-size-larger link"> Kolpingjugend Diözesanverband Rottenburg-Stuttgart</a> teil.<br>
+                Zusätzlich zu unseren Gruppenstunden nehmen wir auch an Aktivitäten der <a href="https://jugend.kolping-dvrs.de" class="text-size-large link"> Kolpingjugend Diözesanverband Rottenburg-Stuttgart</a> teil.<br>
                 Dort treffen wir andere freshe Kolpingjugenden, lernen neues und haben natürlich auch viel Spaß.<br>
                 Mehrere Jugendliche unserer Kolpingjugend sind außerdem Ehrenamtlich auf Diözesanebene tätig und Planen Aktionen wie das Jugendfestival mit.<br>
+                <!-- Bild von uns allen -->
             </span>
         </div>
     </div>
