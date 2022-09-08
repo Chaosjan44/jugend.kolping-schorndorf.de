5c95e022f7239e5c358a651d0cb1118807cbe000
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-09-08 13:27:36 +0200

    Update wir.php

--- wir.php
+++ wir.php
@@ -4,6 +4,11 @@ require_once("templates/header.php"); ?>
 <div class="container py-3">
     <div style="min-height: 80vh;">
         <h1 class="display-3 text-center mb-3 text-kolping-orange">Wir</h1>
+        <div>
+            <span class="text-size-larger">
+                Wir sind die Kolpingjugend Schorndorf, 
+            </span>
+        </div>
     </div>
 </div>
 <?php require_once("templates/footer.php"); ?>
