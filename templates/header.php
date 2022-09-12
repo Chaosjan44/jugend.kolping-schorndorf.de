1c3670a0e2b2b5aa4f0dc06b06767e7d59d1c64b
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-09-12 20:21:17 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -16,9 +16,9 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/css/styles.css">
-    <link rel="stylesheet" href="/css/dark.css" disabled>
-    <link rel="stylesheet" href="/css/light.css" disabled>
+    <link rel="stylesheet" href="/css/styles.css">          <!-- Link Stylesheet -->
+    <link rel="stylesheet" href="/css/dark.css" disabled>   <!-- Link Dark Stylesheet and disable it -->
+    <link rel="stylesheet" href="/css/light.css" disabled>  <!-- Link Light Stylesheet and disable it -->
     <script src="/js/custom.js"></script>
     <script src="/js/markdown_unmark.js"></script>
     <link rel="icon" type="image/png" href="/favicon.png" sizes="1024x1024" />
