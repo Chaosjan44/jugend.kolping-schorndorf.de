238d0189a08c4368d16e46a776932cf48347b437
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-09-14 12:37:08 +0200

    Joa

--- php/functions.php
+++ php/functions.php
@@ -45,7 +45,17 @@ function check_user() {
 	}
 }
 
-
+function sqlError($error_msg) {
+	global $pdo;
+	$backtrace = debug_backtrace();
+	if (!empty($error_log)) {
+		error_log($backtrace[count($backtrace)-1]['file'] . ':' . $backtrace[count($backtrace)-1]['line'] . ': ' . $error_msg . ': ' . $error_log);
+	} else {
+		error_log($backtrace[count($backtrace)-1]['file'] . ':' . $backtrace[count($backtrace)-1]['line'] . ':' . $error_msg);
+	}
+	include_once("templates/error.php");
+	exit;
+}
 
 
 
