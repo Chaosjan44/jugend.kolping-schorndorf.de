6da81b7268446444d6ec79f3b6a4ac6bd1aacd00
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-09-16 12:12:04 +0200

    Logo Changs

--- templates/header.php
+++ templates/header.php
@@ -16,7 +16,7 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" >
     <link rel="stylesheet" href="/css/styles.css">          <!-- Link Stylesheet -->
     <link rel="stylesheet" href="/css/dark.css" disabled>   <!-- Link Dark Stylesheet and disable it -->
     <link rel="stylesheet" href="/css/light.css" disabled>  <!-- Link Light Stylesheet and disable it -->
@@ -48,10 +48,9 @@ session_start();
                 <li class="nav-item text-size-x-large">
                     <a class="nav-link clink clink" href="/wir.php">Wir</a>
                 </li>
-                <li class="nav-item text-size-x-large <?php if (!isMobile()) print("ps-2");?>">
+                <li class="nav-item text-size-x-large <?php if (!isMobile()) print("ps-2"); else print("pt-2");""?>">
                     <a href="https://kolping-schorndorf.de">
-                        <img src="/images/Kolping_logo.png" class="navbar-kolping_light align-text-bottom pe-2" alt="Kolping Logo">
-                        <img src="/images/Kolping_logo.png" class="navbar-kolping_dark align-text-bottom pe-2" alt="Kolping Logo">
+                        <img src="/images/Kolping_logo_n.png" class="navbar-kolping align-text-bottom pe-2" alt="Kolping Logo" style="height: 50px;">
                     </a>
                 </li>
             </ul>
