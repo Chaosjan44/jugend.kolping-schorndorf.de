bf81d515a651bcd2a3852e7b1469beb60ad23e0c
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-10-27 19:33:37 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -54,6 +54,7 @@ function showPreview(event){
 	preview.innerHTML = '';
   var files1 = files
   var counter = 0;
+  var f;
 	for (i = 0, f; f = files[i]; i++) { 
     var reader = new FileReader();
     var hash;
@@ -61,7 +62,7 @@ function showPreview(event){
     reader.onload = function(event) {
       var binary = event.target.result;
       hash = md5(binary).toString();
-      f = f1[counter];
+      var f = f1[counter];
       preview.innerHTML += ['<div class="col" id="', hash ,'"><div class="card prodcard cbg"><img src="', URL.createObjectURL(f), '" class="card-img-top img-fluid rounded" title="', escape(f.name), '" alt="', escape(f.name), '"><div class="card-body"><input type="number" value="1" name="blog_image_id-', hash ,'" style="display: none;" required><div class="input-group pb-2"><span class="input-group-text" id="basic-addon1">Quelle</span><input type="text" class="form-control" placeholder="Quelle" value="" name="imgOwner-', hash ,'"></div><div class="input-group py-2"><span class="input-group-text" id="basic-addon1">Text</span><input type="text" class="form-control" placeholder="Text" value="" name="imgAlt-', hash ,'"></div><div class="input-group pt-2 d-flex justify-content-center"><span class="input-group-text" for="inputVisible">Vorschau Bild</span><div class="input-group-text"><input type="checkbox" class="form-check-input checkbox-kolping" value="" name="prevImg-', hash ,'"></div></div></div></div></div>'].join('');
       counter++;
     };
