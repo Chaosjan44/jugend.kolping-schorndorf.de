620bab1c7e8b4edf14f800a956f36f606efc5935
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-11-20 15:35:14 +0100

    small logo fix

--- templates/header.php
+++ templates/header.php
@@ -34,8 +34,8 @@ session_start();
     <nav class="navbar navbar-expand-lg cbg ctext">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">
-                <img src="/images/Kolpingjugend_light.svg" class="navbar-icon_light align-text-bottom pe-2" alt="Navbar Logo">
-                <img src="/images/Kolpingjugend_dark.svg" class="navbar-icon_dark align-text-bottom pe-2" alt="Navbar Logo">
+                <img src="/images/Kolpingjugend_light.svg" class="navbar-icon_light" alt="Navbar Logo">
+                <img src="/images/Kolpingjugend_dark.svg" class="navbar-icon_dark" alt="Navbar Logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -53,8 +53,8 @@ session_start();
                     </li>
                     <li class="nav-item text-size-x-large <?php if (!isMobile()) print("ps-2 pt-2");?>">
                         <a href="https://kolping-schorndorf.de" target="_blank">
-                            <img src="/images/KolpingK.jpg" class="navbar-icon_light_k align-text-bottom pe-2" alt="Navbar Logo">
-                            <img src="/images/KolpingK_dark.png" class="navbar-icon_dark_k align-text-bottom pe-2" alt="Navbar Logo">
+                            <img src="/images/KolpingK.jpg" class="navbar-icon_light_k" alt="Navbar Logo">
+                            <img src="/images/KolpingK_dark.png" class="navbar-icon_dark_k" alt="Navbar Logo">
                         </a>
                     </li>
                 </ul>
