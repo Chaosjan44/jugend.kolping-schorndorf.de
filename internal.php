32e1b9501281ae4e6241770bb06aa4c4635c3213
Chaosjan44 <j.m.schniebs@gmail.com>
2023-07-22 18:49:32 +0200

    made it so that nextcloud opens in another tab

--- internal.php
+++ internal.php
@@ -31,7 +31,7 @@ echo $buffer;
                         <?php if ($user['perm_event'] == "1"): ?>
                             <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.location.href = '/internal/termine.php';">Termine</button>
                         <?php endif; ?>
-                        <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.location.href = 'https://daten.kolping-schorndorf.de';">Nextcloud</button>
+                        <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.open('https://daten.kolping-schorndorf.de','_blank')">Nextcloud</button>
                         <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.location.href = '/internal/settings.php';">Einstellungen</button>
                         <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.location.href = '/internal/logout.php';">Abmelden</button>
                     </div>
@@ -47,7 +47,7 @@ echo $buffer;
                     <?php if ($user['perm_event'] == "1"): ?>
                         <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.location.href = '/internal/termine.php';">Termine</button>
                     <?php endif; ?>
-                        <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.location.href = 'https://daten.kolping-schorndorf.de';">Nextcloud</button>
+                        <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.open('https://daten.kolping-schorndorf.de','_blank')">Nextcloud</button>
                     <div class="card-text">
                         <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.location.href = '/internal/settings.php';">Einstellungen</button>
                         <button class="btn btn-kolping mx-1 my-2" type="button" onclick="window.location.href = '/internal/logout.php';">Abmelden</button>
